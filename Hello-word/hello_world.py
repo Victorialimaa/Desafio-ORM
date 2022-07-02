c6502881e2823129e060c3a6c7559a7683572889
victoria lima <victoriasousa256@gmail.com>
2022-07-02 15:25:00 -0300

    minha primeira atividade em python

---
+++ Hello-word/hello_world.py
@@ -0,0 +1,84 @@
+def helloo_world():
+  # TODO: Retorne a string 'Olá Mundo' e mostre para todos que vocẽ esta On!!
+  #print("Olá Mundo")
+  return ("hello world")
+  #return 'Olá Mundo'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+#roupa = "branca"
+#if roupa == "preta":
+#  print ('temos camisetas.')
+#elif roupa == "azul":
+#  print ('nao temos camisetas')
+#else:
+#  print ('nao existente')
+
+  
+#ano =int(input( "qual ano você nasceu?"))
+#idade = 2022 - ano
+#if idade >= 18:
+  #print ('Você é adulto') 
+#else:
+   #print ('Não é adulto')
+
+#import time
+#contador = 0
+#while contador <10:
+  #print ('ainda não deu')
+  #contador = contador +1
+  #if contador == 6:
+   #break
+  #time.sleep(1)
+#print ('agora deu')
+
+#numero = int(input('informe um numero:'))
+#numero = numero
+#contador = 1
+#while (numero-contador)>1:
+  #fatorial = fatorial*(numero-contador)
+  #contador += 1
+#print ('{0}!) = {1}'.format(numero,fatorial))
+
+
+
+
+
+
+
+
