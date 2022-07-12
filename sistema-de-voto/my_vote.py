c5d0d8c518ca0d842911bafa4cfe796c4fcfa3ee
victoria lima <victoriasousa256@gmail.com>
2022-07-12 17:23:55 -0300

    sistema de voto com python

---
+++ sistema-de-voto/my_vote.py
@@ -0,0 +1,15 @@
+def can_vote(age):
+  # TODO: Sendo informado a idade retorne a informação se a pessoa poderá votar (True) ou não (False)
+  # TODO: Poderão votar pessoas com 18 anos ou mais
+
+  if age >= 18:
+    return True
+  else:
+     return False
+     
+  print (can_vote)
+  
+  #if condição verdadeira
+  #else condição falsa
+  #exemplo: se 8=8 = 16 >> if
+  #exemplo: se 8=8 = 17 >> else
