a1bef2f44d4c9b2abf047097dee51e73f92a7c74
victoria lima <victoriasousa256@gmail.com>
2022-07-12 17:15:47 -0300

    calculadora em python

---
+++ calculadora-python/calculator.py
@@ -0,0 +1,6 @@
+def calculator():
+ # TODO: Retorne o resultado da adição dos números informados pelo usuário
+ pass
+ n1 = int(input('digite o primeiro numero:'))
+ n2 = input('digite o segundo numero:')
+ return n1 + n2
